dce725f7f6f2d4e16741fe078d5e3e93fa08e132
Jaber Hossain Shovon <jaberjsr@gmail.com>
2024-07-13 11:11:32 +0600

    les: 6 - Dynamic Routes

---
+++ src/routes/products/[productID]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {page} from '$app/stores'
+    const productID = $page.params.productID;
+    console.log($page);
+    
+</script>
+
+<h1>Product Details about product {productID}</h1>
