904f3c09bbbd043398e53460b672df36daefb877
Jaber Hossain Shovon <jaberjsr@gmail.com>
2024-07-15 07:16:23 +0600

    les: 9 - Optional Parameters

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+    const grettings = {
+        en : "Hello",
+        es : "Hola",
+        bn: "ওহে"
+    }
+
+    const {lang = 'en'} = $page.params
+    
+    const gretting = grettings[lang]
+    console.log(gretting);
+</script>
+
+<h1>{gretting}</h1>
