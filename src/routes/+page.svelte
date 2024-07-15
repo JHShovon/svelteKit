45d739951f05d9e313c8806b08038c6bcabce60e
Jaber Hossain Shovon <jaberjsr@gmail.com>
2024-07-15 10:57:20 +0600

    les: 10 - Route Navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
 <h1>Home page</h1>
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/blog">blog</a></li>
+    <li><a href="/products">products</a></li>
+</ul>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,4 +1,10 @@
-<h1>Product List</h1>
-<h1>Product 1</h1>
-<h1>Product 2</h1>
-<h1>Product 3</h1>
+<script>
+   export let productID = 100
+</script>
+
+<a href="/">Home</a>
+
+<h1> Product List</h1>
+<h1> <a href="/products/1">Product 1</a> </h1>
+<h1> <a href="/products/2">Product 2</a> </h1>
+<h1> <a href="{`/products/${productID}`}">{productID}</a> </h1>
